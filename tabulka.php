7e44b3efa80086d65906baedb3d421d5fe0bd0cf
RadaTruh <117731977+RadaTruh@users.noreply.github.com>
2023-11-20 19:16:42 +0100

    Update tabulka.php

--- tabulka.php
+++ tabulka.php
@@ -1,14 +1,35 @@
 <!DOCTYPE html>
+<html>
 <head>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: 20px;
+        }
+
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
 </head>
 <body>
+
 <?php
+// Sample data for the table
 $students = array(
     array("John", "Doe", 25),
     array("Jane", "Smith", 22),
     array("Bob", "Johnson", 28)
 );
 
+// Outputting the table
 echo "<table>";
 echo "<tr><th>First Name</th><th>Last Name</th><th>Age</th></tr>";
 
