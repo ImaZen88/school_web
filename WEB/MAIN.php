f23852bbe2466b41079c8cf1fc4bb6626e80e6f2
Jiří Podrazký <81192874+ImaZen88@users.noreply.github.com>
2024-01-09 13:53:54 +0100

    Oprava pacmana

--- WEB/MAIN.php
+++ WEB/MAIN.php
@@ -14,7 +14,7 @@
        <li><p class="p1"><a href="sachy.html">Šachy</a></li>
        <li><p class="p1"><a href="had.html">Had</a></li>
        <li><p class="p1"><a href="pexeso.html">Pexeso</a></li>
-       <li><p class="p1"><a href="pacman.html">Pac-Man</a></li>
+       <li><p class="p1"><a href="Pacman.html">Pac-Man</a></li>
        <li><p class="p1"><a href="../tictactoe/tictactoe_HTML.html">Tic-tac-toe</a></li>
     </ul>
     </nav>
