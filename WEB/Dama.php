b9235090afe2c0f95462ae31edc0fb475e1392c5
Jiří Podrazký <81192874+ImaZen88@users.noreply.github.com>
2024-02-20 13:24:34 +0100

    Oprava nefunkčního Dama.php

---
+++ WEB/Dama.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Dáma</title>
+    <link rel="stylesheet" type="text/css" href="Dama.css">
+  </head>
+  <?php include 'Navbar.php';?>
+  <body>
+    <h1><center>Dáma</center></h1>
+    <p>
+      Dáma je desková hra pro 2 hráče. Cílem je vzít všechny soupeřovy kameny
+      nebo mu zabránit v dalším tahu. Každý hráč má 12 kamenů a pohybují se
+      diagonálně. Mohou být přeměněny na dámu. Hráč může skákat přes soupeřovy
+      kameny a brát je. Hra končí, když jeden hráč ztratí všechny kameny nebo je
+      mu zabráněno v dalším tahu. Remíza nastane, pokud žádný hráč nemůže
+      táhnout.
+    </p>
+
+    <img 
+      src="https://mujsoubor.cz/mobilni-logicke-hry/data/mujsoubor.cz/appimages/07/7602_25336.jpg" id="obrazek1"
+    />
+    <img
+      src="https://th.bing.com/th/id/OIP.A08HIapTqnVOPN7juaL3eQHaEK?pid=ImgDet&rs=1" id="obrazek2"
+      />
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/NuIvfYSNSrk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen id="video"></iframe>
+  </body>
+</html>
